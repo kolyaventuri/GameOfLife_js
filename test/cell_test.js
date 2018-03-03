e3d9c4366fc6d0b894c7a79e9046ab60dca1fbfd
Kolya Venturi <kolya.venturi@enx3s.com>
2018-03-03 15:15:28 -0700

    Cell tests

---
+++ test/cell_test.js
@@ -0,0 +1,30 @@
+const expect = require('chai').expect;
+const pry = require('pryjs');
+
+const Cell = require('../lib/cell');
+
+describe('Cell', () => {
+
+
+  it('should be dead by default', () => {
+    let cell = new Cell();
+    expect(cell.alive).to.be.false;
+  });
+
+  it('should be able to be alive by default', () => {
+    let cell = new Cell(true);
+    expect(cell.alive).to.be.true;
+  });
+
+  it('should be able to die', () => {
+    let cell = new Cell(true);
+    expect(cell.die).to.be.undefined;
+    expect(cell.alive).to.be.false;
+  });
+
+  it('should be able to become alive', () => {
+    let cell = new Cell();
+    expect(cell.live).to.be.undefined;
+    expect(cell.alive).to.be.true;
+  })
+});
