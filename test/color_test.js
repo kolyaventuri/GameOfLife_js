5dec8c8539e64668f0c8251c01f4c1b57b1191a8
Kolya Venturi <kolya.venturi@enx3s.com>
2018-03-07 18:50:35 -0600

    Genericized test to just be between colors based on percentage

--- test/color_test.js
+++ test/color_test.js
@@ -4,50 +4,31 @@ const pry = require('pryjs');
 const Color = require('../lib/color');
 
 describe('Color', () => {
-  let color = new Color();
+  let color = new Color(
+    [120, 100, 50],
+    [0, 0, 75]
+  );
 
-  it('should be green by default', () => {
-    expect(color.hue).to.equal(120);
+  it('should have a start and end color', () => {
+    expect(color.start).to.eql([120, 100, 50]);
+    expect(color.end).to.eql([0, 0, 75]);
   });
 
-  it('should be able to start with different hue', () => {
-    let _color = new Color(50);
-    expect(_color.hue).to.equal(50);
-  });
-
-  it('should have default attributes', () => {
-    expect(color.timeToGrey).to.equal(5);
-    expect(color.maxL).to.equal(100);
-    expect(color.minS).to.equal(0);
-  });
-
-  it('should be able to set attributes', () => {
-    color.setTimeToGrey(10);
-    color.setMaxL(75);
-    color.setMinS(20);
-
-    expect(color.timeToGrey).to.equal(10);
-    expect(color.maxL).to.equal(75);
-    expect(color.minS).to.equal(20);
-  });
-
-  it('should calculate HSL values', () => {
-    color.setTimeToGrey(10);
-    color.setMaxL(75);
-
+  it('should be able to calculate steps by percentage', () => {
     let _color = color.calculate(0);
     expect(_color).to.eql([120, 100, 50]);
 
-    _color = color.calculate(5);
-    expect(_color).to.eql([120, 50, 62.5]);
+    _color = color.calculate(0.25);
+    expect(_color).to.eql([90, 75, 56.25]);
 
-    _color = color.calculate(10);
-    expect(_color).to.eql([120, 0, 75]);
+    _color = color.calculate(0.5);
+    expect(_color).to.eql([60, 50, 62.5]);
 
-    _color = color.calculate(11);
-    expect(_color).to.eql([120, 0, 75]);
+    _color = color.calculate(0.75);
+    expect(_color).to.eql([30, 25, 68.75]);
 
-    _color = color.calculate(7);
-    expect(_color).to.eql([120, 30, 67.5]);
+    _color = color.calculate(1);
+    expect(_color).to.eql([0, 0, 75]);
   });
+
 });
